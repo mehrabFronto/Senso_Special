45499def045a9bbe6f8dd58d2ed215d581b4ba44
mehrabFronto <mehrabd20@gmail.com>
2024-12-17 10:09:30 +0330

    feat: develop searchbar

---
+++ src/app/_components/SearchBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+
+const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  return (
+    <section className="px-md">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-full flex items-center"
+      >
+        <input
+          type="text"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="w-full bg-[#3A3A3A] rounded-r-lg p-2.5 placeholder:text-[#777] border border-[#777] border-l-0 outline-none"
+          placeholder="برای جستجو قسمتی از نام محصول را بنویسید..."
+        />
+
+        <button
+          type="submit"
+          title="search"
+          className="bg-primary border border-primary rounded-l-lg overflow-hidden py-1 px-2"
+        >
+          <Image
+            src="/icons/search.svg"
+            alt="search"
+            width={50}
+            height={50}
+            className="w-9 h-9"
+          />
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default SearchBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./_components/Header";
+import SearchBar from "./_components/SearchBar";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Header />
+      <SearchBar />
     </div>
   );
 };
