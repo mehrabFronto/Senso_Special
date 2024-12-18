7b449285ae11784ae98fafa0373ee52a6543400b
mehrabFronto <mehrabd20@gmail.com>
2024-12-18 18:17:03 +0330

    feat(about-page): develop page

---
+++ src/app/about/_components/MapComponent.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const customIcon = L.icon({
+  iconUrl: "/icons/map-marker-green.png",
+  iconSize: [42, 42],
+  popupAnchor: [0, -20],
+});
+
+const MapComponent = () => {
+  return (
+    <div className="h-[300px] w-full">
+      <MapContainer
+        center={[35.7209559, 50.987012]}
+        zoom={16}
+        scrollWheelZoom={false}
+        zoomControl={false}
+        className="h-full w-full rounded-lg"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        />
+        <Marker position={[35.7209559, 50.987012]} icon={customIcon}>
+          <Popup>رستوران ایتالیایی حس خاص</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapComponent;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import dynamic from "next/dynamic";
+
+const MapComponent = dynamic(() => import("./_components/MapComponent"));
 
 const AboutPage = () => {
   return (
-    <main className="min-h-[80vh] flex items-center justify-center text-primary text-3xl font-bold@">
-      درباره ما
+    <main className="p-2xl pb-4xl max-w-screen-lg mx-auto">
+      <div className="w-full space-y-xl bg-[#2A2A2A] p-xl rounded-xl border border-[#5A5A5A]">
+        <div className="text-center font-bold flex flex-col items-center gap-y-xs">
+          <p>کافه رستوران ایتالیایی حس خاص</p>
+          <p>کرج، فردیس، فلکه سوم، خیابان سی و یکم(دهم غربی قدیم)</p>
+          <p>026-36502521</p>
+          <p>
+            <span className="text-primary">Instagram:</span> senso_speciale
+          </p>
+        </div>
+
+        <MapComponent />
+      </div>
     </main>
   );
 };
